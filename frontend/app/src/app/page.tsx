36250a35531c06cdf361085891e381513d0bf768
sim0wastaken <104425606+sim0wastaken@users.noreply.github.com>
2024-12-02 16:05:53 +0100

    Grazie poiso per questo hotfix

--- frontend/app/src/app/page.tsx
+++ frontend/app/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
